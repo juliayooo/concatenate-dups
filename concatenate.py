a72ddd351560a35aa47abe0332a3da63335f81b7
Events 23A <events23a@Events-23As-MacBook-Pro.local>
2025-08-20 13:42:31 -0400

    seemingly working, account for substring repetition

--- concatenate.py
+++ concatenate.py
@@ -12,20 +12,28 @@ with open(ouput_filepath, mode='w', newline='', encoding='ISO-8859-1') as out_fi
     writer.writerow(['Primary Contact ID', 'Concatenated Description', 'Concatenated Relationship to AFH', 'Date'])
 
 
-today = date.today().strftime("%Y-%m-%d")
+    today = date.today().strftime("%Y-%m-%d")
 
-data = pd.read_csv(filepath, encoding='ISO-8859-1')
-merges = pd.read_csv(merges_filepath, encoding='ISO-8859-1')
+    data = pd.read_csv(filepath, encoding='ISO-8859-1')
+    merges = pd.read_csv(merges_filepath, encoding='ISO-8859-1')
 
-for group in merges.groupby('Duplicate Group'):
-    concat_desc  = ""
-    concat_afh = ""
-    prim_id = ""
-    for c_id in group[1]['Contact ID']:
-        concat_desc += data[data['Contact ID'] == c_id]['Description'].values[0] + "\n"
-        concat_afh += data[data['Contact ID'] == c_id]['Relationship to AFH'].values[0] + "\n"
-        if data[data['Contact ID'] == c_id]['Primary Record'].values[0] == 'Primary Record' or data[data['Contact ID'] == c_id]['Primary Record'].values[0] == 1:
-            prim_id = c_id
+    for group in merges.groupby('Duplicate Group'):
+        concat_desc  = ""
+        concat_afh = ""
+        prim_id = ""
+        for c_id in group[1]['Contact ID']:
+            desc = data.loc[data['Contact ID'] == c_id, 'Description'].fillna("").values
+            afh  = data.loc[data['Contact ID'] == c_id, 'Relationship to AFH'].fillna("").values
 
-    out_file.write([prim_id, concat_desc, concat_afh, today])
-    
+            if len(desc) > 0:
+                concat_desc += desc[0] + " "
+            if len(afh) > 0 and (afh[0] not in concat_afh):
+                concat_afh += afh[0] + "; "
+            # concat_desc += str(str(data[data['Contact ID'] == c_id]['Description'].values[0]) + "\n")
+            # concat_afh += str(str(data[data['Contact ID'] == c_id]['Relationship to AFH'].values[0]) + "\n")
+            if merges[merges['Contact ID'] == c_id]['Primary Record'].values[0] == 'Primary Record' or merges[merges['Contact ID'] == c_id]['Primary Record'].values[0] == 1:
+                prim_id = c_id
+        if prim_id == "":
+            continue
+
+        writer.writerow([prim_id, concat_desc, concat_afh, today])
